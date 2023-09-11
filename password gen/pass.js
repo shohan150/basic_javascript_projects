6df72b89c108f8b595c615d447960c79891f3ed2
shohan <shohan.ece17@gmail.com>
2023-09-11 11:45:37 +0600

    password generator

---
+++ password gen/pass.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById('password');
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = '0123456789';
+const symbol = "!@#$%&*/";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = '';
+    // password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    // password += lowerCase[Math.floor(Math.random() * upperCase.length)];
+    // password += number[Math.floor(Math.random() * number.length)];
+    // password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    password.select(); // Select the text in the input element with the id "password"
+    document.execCommand('copy'); // Execute the 'copy' command to copy the selected text to the clipboard
+}
