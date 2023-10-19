a51e361b8407c76e05806ca8ddee38c17daa6e6a
shohan <shohan.ece17@gmail.com>
2023-10-19 22:40:22 +0600

    made a ad blocked (partially effective) in the chore ext repo

--- Notes app/notes.js
+++ Notes app/notes.js
@@ -1,9 +1,9 @@
 const notesContainer = document.querySelector('.notes-container');
 const createBtn = document.querySelector('.btn');
-let notes = document.querySelector('input-box');   //try using const. const dile notun note kore entry dao. refresh. gone. 2 ta notun entry diye 1 ta delete dao. refresh. oi 1 ta ache. karon code dekhlei bujhte parba. delete r paragraphn on key er store korar code alada. 2nd ta running code e notes k update korte chai jeta const e possible na. But still I need to clear the concept more. 
+// let notes = document.querySelector('input-box');   //try using const. const dile notun note kore entry dao. refresh. gone. 2 ta notun entry diye 1 ta delete dao. refresh. oi 1 ta ache. karon code dekhlei bujhte parba. delete r paragraphn on key er store korar code alada. 2nd ta running code e notes k update korte chai jeta const e possible na. But still I need to clear the concept more. 
 
 createBtn.addEventListener('click', () => {
-    let inputBox = document.createElement('p');  //used let in tutorial
+    let inputBox = document.createElement('div');  //used let in tutorial
     let img = document.createElement('img');  //used let in tutorial
     inputBox.className = 'input-box';
     inputBox.setAttribute('contenteditable', 'true');
@@ -23,18 +23,22 @@ function updateStorage() {
 
 notesContainer.addEventListener('click', function (e) {
     if (e.target.tagName === 'IMG') {
-        e.target.parentElement.remove();
-        updateStorage();
+        if (e.target.parentElement.parentElement.tagName === 'DIV') {
+            e.target.parentElement.parentElement.remove();
+            updateStorage();
+        }
+        else {
+            e.target.parentElement.remove();
+            updateStorage()
+        }
     }
-    // sets up an event listener for a click event on an element with the id "notesContainer" (presumably a container for notes or elements). When a click event occurs within this container, the code checks if the target of the click event is an IMG (image) element. If it is, it removes the parent element of the image, effectively removing the image.
+    // sets up an event listener for a click event on an element with the id "notesContainer". When a click event occurs within this container, the code checks if the target of the click event is an IMG (image) element. If it is, it removes the parent element of the image, effectively removing the image.
 
-    // .addEventListener('click', function (e) { ... }): This code adds a click event listener to the "notesContainer" element. When a click event occurs within this container, the function inside the event listener is executed. The e parameter represents the event object.
-
-    // if (e.target.tagName === 'IMG') { ... }: Inside the event listener, it checks if the target of the click event (e.target) is an IMG element. The tagName property of the target element returns its tag name in uppercase.
+    // .addEventListener('click', function (e) { ... }): This code adds a click event listene. The e parameter represents the event object. if (e.target.tagName === 'IMG') { ... }: Inside the event listener, it checks if the target of the click event (e.target) is an IMG element. The tagName property of the target element returns its tag name in uppercase.
 
     // e.target.parentElement.remove();: If the target element is an IMG, it accesses its parent element (the element that contains the image), and then it calls the remove method on the parent element. This effectively removes the parent element (and consequently, the image) from the DOM, effectively deleting it.
 
-    else if (e.target.tagName === 'P') {
+    else if (e.target.tagName === 'DIV') {
         notes = document.querySelectorAll('.input-box'); //querySelector is used to select the first element that matches a given CSS selector. querySelectorAll is used to select all elements that match a given CSS selector.
         notes.forEach(nt => {
             nt.onkeyup = function () {
@@ -50,9 +54,7 @@ document.addEventListener('keydown', event => {
         document.execCommand('insertLineBreak');
         event.preventDefault();
     }
-    // When a key is pressed down, the event handler checks if the pressed key is the "ENTER" key (usually the "Enter" or "Return" key on the keyboard).
-
-    // If the "ENTER" key is detected, the code executes two actions:
+    // When a key is pressed down, the event handler checks if the pressed key is the "ENTER" key (usually the "Enter" or "Return" key on the keyboard). If the "ENTER" key is detected, the code executes two actions:
 
     // document.execCommand('insertLineBreak'): This line invokes the execCommand method with the argument 'insertLineBreak.' This command is used to insert a line break at the current cursor position. In essence, it simulates pressing the "Enter" key, which would create a new line in a text input or textarea.
 
--- quotes/quotes.js
+++ quotes/quotes.js
@@ -6,6 +6,8 @@ const author = document.getElementById('author');
 async function getQuote(url) {
     const responce = await fetch(url);
     var data = await responce.json();
+    console.log(typeof (responce));
+    console.log(responce);
     console.log(data);
     quote.innerHTML = data.content;
     author.innerHTML = data.author;
@@ -16,5 +18,5 @@ getQuote(api_url);
 
 function tweet() {
     window.open(
-        'https://twitter.com/intent/tweet?text=' + quote.innerHTML + ' ---By ' + author.innerHTML, 'Sharing the Quote on X', 'width=1200,height=400');
+        'https://twitter.com/intent/tweet?text=' + quote.innerHTML + ' ---By ' + author.innerHTML, 'Sharing the Quote on X', 'width=650,height=400');
 }
